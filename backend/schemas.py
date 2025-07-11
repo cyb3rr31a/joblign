4e5f87372d28a89d6dcc3206b65a12a3a9ae17ce
Rebecca Shirievo <139311697+cyb3rr31a@users.noreply.github.com>
2025-07-11 16:08:50 +0300

    add: add job tracking database

---
+++ backend/schemas.py
@@ -0,0 +1,33 @@
+from pydantic import BaseModel
+from typing import Optional
+from datetime import datetime
+
+class JobCreate(BaseModel):
+    title: str
+    company: str
+    location: Optional[str] = None
+    job_description : Optional[str] = None
+    appication_link: Optional[str] = None
+    notes: Optional[str] = None
+
+class JobUpdate(BaseModel):
+    status: Optional[str] = None
+    resume_score: Optional[int] = None
+    ai_feedback: Optional[str] = None
+    notes: Optional[str] = None
+
+class JobOut(BaseModel):
+    id: int
+    title: str
+    company: str
+    location: Optional[str]
+    status: str
+    resume_store: Optional[str]
+    ai_feedback: Optional[str]
+    job_description: Optional[str]
+    application_link: Optional[str]
+    notes: Optional[str]
+    created_at: datetime
+
+class Config:
+    orm_mode = True
