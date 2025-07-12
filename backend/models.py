edc4503f823dbbb04fe426b7cae36e8251a396a8
Rebecca Shirievo <139311697+cyb3rr31a@users.noreply.github.com>
2025-07-12 10:10:24 +0300

    add: user class

--- backend/models.py
+++ backend/models.py
@@ -1,7 +1,15 @@
-from sqlalchemy import Column, Integer, String, Text, DateTime
+from sqlalchemy import Column, Integer, String, Text, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 from database import Base
 
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    jobs = relationship("Job", back_populates="owner")
 class Job(Base):
     __tablename__ = "jobs"
 
